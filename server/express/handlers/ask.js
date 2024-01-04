d6e55a0f1231ef0b30dbecb0cbcdf915e1b90ea4
Petru-Teodor Ștefănescu <stefanescupetru@yahoo.com>
2024-01-04 15:38:34 +0200

    [2] Made a basic prompt processing

--- server/express/handlers/ask.js
+++ server/express/handlers/ask.js
@@ -1,3 +1,8 @@
+const linkWords = new Set([
+	"how", "help", "and", "or", "can", "i", "you", "he", "she", "we", "they", "do",
+	"this", "that"
+]);
+
 module.exports = function (req, res) {
 	const { prompt } = req.query;
 	if (!prompt) {
@@ -5,7 +10,8 @@ module.exports = function (req, res) {
 			"error": "Missing prompt"
 		});
 	}
-	res.json({
-		prompt
+	const usefulPrompt = prompt.toLowerCase().split(" ").filter(word => !linkWords.has(word)).join(" ");
+	return res.json({
+		prompt: usefulPrompt
 	});
 };

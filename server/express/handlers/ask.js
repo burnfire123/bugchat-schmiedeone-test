6bedbd8aaa65d96bf42597a99b435f0fc80ad91a
Petru-Teodor Ștefănescu <stefanescupetru@yahoo.com>
2024-01-04 15:30:23 +0200

    [2] Handled missing prompt error

--- server/express/handlers/ask.js
+++ server/express/handlers/ask.js
@@ -1,5 +1,10 @@
 module.exports = function (req, res) {
 	const { prompt } = req.query;
+	if (!prompt) {
+		return res.status(412).send({
+			"error": "Missing prompt"
+		});
+	}
 	res.json({
 		prompt
 	});

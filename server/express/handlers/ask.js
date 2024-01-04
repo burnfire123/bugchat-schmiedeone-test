5fd26d27f2b9cd0076d17aeca867044f5098646f
Petru-Teodor Ștefănescu <stefanescupetru@yahoo.com>
2024-01-04 16:06:00 +0200

    [2] Treated missing Wikipedia page

--- server/express/handlers/ask.js
+++ server/express/handlers/ask.js
@@ -1,16 +1,38 @@
+const { default: axios } = require("axios");
+
 const linkWords = new Set([
 	"how", "help", "and", "or", "can", "i", "you", "he", "she", "we", "they", "do",
-	"this", "that"
+	"this", "that", "about"
 ]);
 
-module.exports = function (req, res) {
+module.exports = async function (req, res) {
 	const { prompt } = req.query;
 	if (!prompt) {
 		return res.status(412).send({
 			"error": "Missing prompt"
 		});
 	}
-	const usefulPrompt = prompt.toLowerCase().split(/[ ,.!?]/).filter(word => !linkWords.has(word)).join(" ");
+	const usefulPrompt = prompt
+		.toLowerCase()
+		.split(/[ ,.!?]/)
+		.filter(word => !linkWords.has(word))
+		.join(" ");
+	// We will use Wikipedia's API to find a page
+	const { data: { query: { pages }} } = await axios.get("https://en.wikipedia.org/w/api.php", {
+		params: {
+			action: "query",
+			titles: usefulPrompt,
+			prop: "extracts",
+			format: "json",
+			exintro: true,
+			explaintext: true,
+		}
+	});
+	if (pages[-1]) {
+		return res.status(404).send({
+			"error": "No page found"
+		});
+	}
 	return res.json({
 		prompt: usefulPrompt
 	});
